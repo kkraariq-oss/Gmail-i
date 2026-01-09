addeb793adb1bd33d3318b16937227b3e46b32f1
kkraariq-oss <kkraariq@gmail.com>
2026-01-09 10:23:23 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,8 @@
-const CACHE_NAME = 'gmail-chat-v4-ultimate';
+const CACHE_NAME = 'gmail-chat-v5-fixed';
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
-  'https://www.gstatic.com/firebasejs/10.7.1/firebase-database-compat.js'
+  './manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
@@ -19,20 +16,5 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 });
